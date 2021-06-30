32ec58c742313949837b2a97acfdc3e74df364b6
Rafael Piacsek <rafaelpiacsek@gmail.com>
2021-06-30 00:52:47 -0500

    HAL training

--- results/ISC_Discrete_Trainer.py
+++ results/ISC_Discrete_Trainer.py
@@ -19,7 +19,7 @@ from torch.cuda import is_available
 config = Config()
 
 config.environment = Wrapper(SimpleISC(mode="DISCRETE"))
-config.num_episodes_to_run = 50
+config.num_episodes_to_run = 1_000
 
 config.file_to_save_data_results = "data_and_graphs/isc/IllinoisSolarCar_Results_Data.pkl"
 config.file_to_save_results_graph = "data_and_graphs/isc/IllinoisSolarCar_Results_Graph.png"
@@ -39,18 +39,18 @@ config.wandb_log = True
 
 config.hyperparameters = {
     "DQN_Agents": {
-        "learning_rate": 0.005,
+        "learning_rate": 0.01,
         "batch_size": 128,
-        "buffer_size": 100000,
+        "buffer_size": 1_000_000,
         "epsilon": 1.0,
-        "epsilon_decay_rate_denominator": 150,
+        "epsilon_decay_rate_denominator": 100,
         "discount_rate": 0.999,
         "alpha_prioritised_replay": 0.6,
         "beta_prioritised_replay": 0.1,
         "incremental_td_error": 1e-8,
         "update_every_n_steps": 15,
         "tau": 1e-2,
-        "linear_hidden_units": [256, 256],
+        "linear_hidden_units": [64, 64, 64, 64, 64],
         "final_layer_activation": "softmax",
         # "y_range": (-1, 14),
         "batch_norm": False,
