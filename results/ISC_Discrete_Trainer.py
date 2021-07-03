ecb14d2bf5d16c93fdb3c629283ce290d292d1d6
Rafael Piacsek <rafaelpiacsek@gmail.com>
2021-07-03 15:35:02 -0500

    HAL training changes

--- results/ISC_Discrete_Trainer.py
+++ results/ISC_Discrete_Trainer.py
@@ -19,7 +19,7 @@ from torch.cuda import is_available
 config = Config()
 
 config.environment = Wrapper(SimpleISC(mode="DISCRETE"))
-config.num_episodes_to_run = 1_000
+config.num_episodes_to_run = 10_000
 
 config.file_to_save_data_results = "data_and_graphs/isc/IllinoisSolarCar_Results_Data.pkl"
 config.file_to_save_results_graph = "data_and_graphs/isc/IllinoisSolarCar_Results_Graph.png"
@@ -43,14 +43,14 @@ config.hyperparameters = {
         "batch_size": 128,
         "buffer_size": 1_000_000,
         "epsilon": 1.0,
-        "epsilon_decay_rate_denominator": 100,
+        "epsilon_decay_rate_denominator": 150,
         "discount_rate": 0.999,
         "alpha_prioritised_replay": 0.6,
         "beta_prioritised_replay": 0.1,
         "incremental_td_error": 1e-8,
         "update_every_n_steps": 15,
         "tau": 1e-2,
-        "linear_hidden_units": [64, 64, 64, 64, 64],
+        "linear_hidden_units": [128, 128, 128, 128],
         "final_layer_activation": "softmax",
         # "y_range": (-1, 14),
         "batch_norm": False,
@@ -65,7 +65,7 @@ config.model = FCNN()
 
 if __name__== '__main__':
     AGENTS = [DQN, DDQN, Dueling_DDQN, DDQN_With_Prioritised_Experience_Replay]
-
+    
     trainer = Trainer(config, AGENTS)
     trainer.train()
 
